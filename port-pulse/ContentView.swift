f246eb4421630adba10c837d5241f718aa8d58f3
Wayne <fssher107@gmail.com>
2023-11-21 11:26:51 +0800

    Initial Commit

---
+++ port-pulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  port-pulse
+//
+//  Created by 賴偉恩 on 2023/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ port-pulse/port_pulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  port_pulseApp.swift
+//  port-pulse
+//
+//  Created by 賴偉恩 on 2023/11/21.
+//
+
+import SwiftUI
+
+@main
+struct port_pulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
